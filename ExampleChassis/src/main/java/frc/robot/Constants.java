b6b0b5183dec58d9453feda457f50b6daa88c46c
Derek Geng <dxiageng@gmail.com>
2020-12-03 17:54:40 -0500

    init commit

---
+++ ExampleChassis/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final int kLeftMaster = 1;
+    public static final int kLeftSlaveA = 2;
+    public static final int kLeftSlaveB = 3;
+
+    public static final int kRightMaster = 4;
+    public static final int kRightSlaveA = 5;
+    public static final int kRightSlaveB = 6;
+
+}
---
+++ ExampleChassis/src/main/java/frc/robot/subsystems/Chassis.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Chassis extends SubsystemBase {
+  /**
+   * Creates a new Chassis.
+   */
+  WPI_TalonSRX m_leftMaster;
+  WPI_VictorSPX m_leftSlaveA;
+  WPI_VictorSPX m_leftSlaveB;
+
+  WPI_TalonSRX m_rightMaster;
+  WPI_VictorSPX m_rightSlaveA;
+  WPI_VictorSPX m_rightSlaveB;
+  
+  public Chassis() {
+    m_leftMaster = new WPI_TalonSRX(Constants.kLeftMaster);
+    m_leftSlaveA = new WPI_VictorSPX(Constants.kLeftSlaveA);
+    m_leftSlaveB = new WPI_VictorSPX(Constants.kLeftSlaveB);
+
+    m_rightMaster = new WPI_TalonSRX(Constants.kRightMaster);
+    m_rightSlaveA = new WPI_VictorSPX(Constants.kRightSlaveA);
+    m_rightSlaveB = new WPI_VictorSPX(Constants.kRightSlaveB);
+
+    m_leftSlaveA.follow(m_leftMaster);
+    m_leftSlaveB.follow(m_leftMaster);
+
+    m_rightSlaveA.follow(m_rightMaster);
+    m_rightSlaveB.follow(m_rightMaster);
+  }
+
+  void set(double left, double right){
+    m_leftMaster.set(left);
+    m_rightMaster.set(right);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
